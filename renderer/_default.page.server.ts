3a34f6bd297f3f0b8445b5e340a3cb6defaef257
Alexander Kolov <me@alexkolov.com>
2022-03-18 17:04:04 +0900

    Favicon handling in Safari is weird

--- renderer/_default.page.server.ts
+++ renderer/_default.page.server.ts
@@ -3,7 +3,6 @@ import type { PageContextBuiltIn } from "vite-plugin-ssr"
 
 import { createApp } from "./app"
 import { escapeInject } from "vite-plugin-ssr"
-import favicon from "~/public/favicon.svg"
 import { getPageTitle } from "~/utils/getPageTitle"
 import { globals } from "~/utils/env"
 import { i18n } from "~/utils/i18n"
@@ -26,7 +25,12 @@ async function render(pageContext: PageContextBuiltIn & PageContext) {
     <html lang="en">
       <head>
         <meta charset="UTF-8"/>
-        <link rel="icon" href="${favicon}"/>
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png"/>
+        <link rel="mask-icon" href="/favicon.svg" color="#ffffff"/>
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg"/>
+        <link rel="manifest" href="/site.webmanifest"/>
         <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0"/>
         <meta name="description" content="${desc}"/>
         <meta property="og:image" content="${globals.baseURL}/opengraph.png"/>
